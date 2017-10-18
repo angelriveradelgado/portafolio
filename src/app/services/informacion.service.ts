866fa49cd5cb809fa80977ed905a26325eee5af4
Angel Rivera <riveradelgadoangel@gmail.com>
2017-10-17 21:27:16 -0500

    Firebase

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,12 +6,26 @@ export class InformacionService {
 
   info: any = {};
   infoCargada = false;
+  infoAboutCargada = false;
+  equipo: any[] = [];
 
   constructor( public http: Http) {
+    this.loadInfo();
+    this.loadAbout();
+  }
+
+  public loadInfo() {
     this.http.get('assets/data/info.pagina.json').subscribe( data => {
-      console.log(data.json());
-      this.info = true;
+      // console.log(data.json());
+      this.infoCargada = true;
       this.info = data.json();
+    });
+  }
+  public loadAbout() {
+    this.http.get('https://portafolio-1059e.firebaseio.com/equipo.json').subscribe( data => {
+      console.log(data.json());
+      this.infoAboutCargada = true;
+      this.equipo = data.json();
     });
   }
 

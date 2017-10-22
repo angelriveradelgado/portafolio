9ed024d344ea8bf89f1e8bb6dc1c31d9b1be0d40
Angel Rivera <riveradelgadoangel@gmail.com>
2017-10-22 14:48:41 -0500

    primera version

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -5,6 +5,7 @@ import { Http } from '@angular/http';
 export class ProductosService {
 
   productos: any[] = [];
+  productosFiltrado: any[] = [];
   cargando = false;
 
   constructor( private http: Http) {
@@ -13,16 +14,48 @@ export class ProductosService {
 
   public cargarProductos() {
     this.cargando = true;
+
+    const promesa = new Promise( (resolve, reject) => {
       this.http.get('https://portafolio-1059e.firebaseio.com/productoIdx.json')
-      .subscribe( res => {
-        // console.log( res.json() );
+        .subscribe( res => {
+          // console.log( res.json() );
 
-        setTimeout( () => {
-          this.cargando = false;
-          this.productos = res.json();
-        }, 1500);
+          setTimeout( () => {
+            this.cargando = false;
+            this.productos = res.json();
+            resolve();
+          }, 1500);
 
+        });
+      } );
+      return promesa;
+
+    }
+
+  public buscarProducto( termino: String ) {
+    if ( this.productos.length === 0 ) {
+      this.cargarProductos().then( () => {
+        // termina la carga
+        this.filtrarProductos( termino );
       });
+
+    } else {
+      this.filtrarProductos( termino );
+    }
+
+  }
+
+  private filtrarProductos( termino: String) {
+
+    this.productosFiltrado = [];
+    termino = termino.toLowerCase();
+
+    this.productos.forEach( prod => {
+      if ( prod.categoria.indexOf( termino ) >= 0 || prod.titulo.toLowerCase().indexOf( termino ) >= 0 ) {
+        this.productosFiltrado.push( prod );
+        // console.log( prod );
+      }
+    });
   }
 
   public cargarProducto( cod: String) {
